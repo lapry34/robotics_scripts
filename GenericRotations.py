6a3af46f8f125e3962d69e7e4fd59a47f4abb594
Gabriele Onorato <onorato.1979543@studenti.uniroma1.it>
2024-10-29 16:00:09 +0100

    Create GenericRotations.py

---
+++ GenericRotations.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+
+PI = np.pi
+
+if __name__ == "__main__":
+    
+    fixed = True
+    order = "ZYX"
+
+    alpha   =   PI/3    #first angle in order
+    beta    =   PI/3    #second angle in order
+    gamma   =   -PI/2   #third angle in order
+    
+    angles = [alpha, beta, gamma]
+    R = np.eye(3)
+
+    if fixed:
+        order = order[::-1]
+        angles = angles[::-1]
+    
+    for c, angle in zip(order, angles):
+        if c == "X":            
+            R_x = np.array([[1, 0, 0],
+                    [0, np.cos(angle), -np.sin(angle)],
+                    [0, np.sin(angle), np.cos(angle)]])
+            R = np.dot(R, R_x)
+
+        elif c == "Y":
+            R_y = np.array([[np.cos(angle), 0, np.sin(angle)],
+                    [0, 1, 0],
+                    [-np.sin(angle), 0, np.cos(angle)]])
+            R = np.dot(R, R_y)
+
+        elif c == "Z":
+            R_z = np.array([[np.cos(angle), -np.sin(angle), 0],
+                    [np.sin(angle), np.cos(angle), 0],
+                    [0, 0, 1]])
+            R = np.dot(R, R_z)
+
+#print R matrix in console with decimal values
+    for row in R:
+        print([round(x, 5) for x in row])
