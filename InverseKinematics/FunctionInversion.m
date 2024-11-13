f5ab53debaeb0ff495facb1b5b0a0793d8714d5e
Gabriele Onorato <onorato.1979543@studenti.uniroma1.it>
2024-11-13 12:28:20 +0100

    ik and central differences

--- InverseKinematics/FunctionInversion.m
+++ InverseKinematics/FunctionInversion.m
@@ -9,14 +9,14 @@ f = @(x) [x(1)^2 + x(2); x(1) + x(2)^2 - 1];  % Example vector-valued function
 delta = 1e-5;  % Small step size
 
 % Target value for which we want to find the inverse
-y = [3; 1];  % Example target vector
+y = [2; 3];  % Example target vector
 
 % Initial guess for x
 x0 = [1; 1];
 x = x0;
 
 % Gradient Descent parameters
-alpha = 0.01;  % Learning rate for gradient descent
+alpha = 0.05;  % Learning rate for gradient descent
 tol = 1e-6;    % Tolerance for convergence
 max_iter = 10000;
 
@@ -37,10 +37,10 @@ end
 
 
 % Display the final solution
-fprintf('The inverse solution of f(x) = [%f; %f] is approximately x = [%f; %f]\n', y(1), y(2), x(1), x(2));
+fprintf('The inverse solution of f(x) = [%f; %f] is approximately x_sol = [%f; %f]\n', y(1), y(2), x(1), x(2));
 
-y = f(x);
-fprintf('f(x_sol) = [%f; %f]\n', y(1), y(2));
+y_sol = f(x);
+fprintf('f(x_sol) = [%f; %f]\n', y_sol(1), y_sol(2));
 
 %reset initial guess
 x = x0;
@@ -52,7 +52,7 @@ for j = 1:max_iter
         fprintf('Newton Method converged in %d iterations\n', j);
         break;
     end
-    x = x - numJ(f,x,delta) \ error;  % Newton's Method update using the Jacobian inverse
+    x = x - alpha * (numJ(f,x,delta) \ error);  % Newton's Method update using the Jacobian inverse
 end
 
 % If Newton's Method did not converge, display a warning
@@ -61,21 +61,24 @@ if j == max_iter
 end
 
 % Display the final solution
-fprintf('The inverse solution of f(x) = [%f; %f] is approximately x = [%f; %f]\n', y(1), y(2), x(1), x(2));
+fprintf('The inverse solution of f(x) = [%f; %f] is approximately x_sol = [%f; %f]\n', y(1), y(2), x(1), x(2));
 
-y = f(x);
-fprintf('f(x_sol) = [%f; %f]\n', y(1), y(2));
+y_sol = f(x);
+fprintf('f(x_sol) = [%f; %f]\n', y_sol(1), y_sol(2));
 
+
+% Define function to compute the numerical Jacobian
 function J = numJ(f, x, delta)
     n = numel(x);
     fx = f(x);
     m = numel(fx);
     J = zeros(m, n);
-    
     for i = 1:n
-        x_step = x;
-        x_step(i) = x_step(i) + delta;
-        J(:, i) = (f(x_step) - fx) / delta;  % Forward difference
+        x_forward = x;
+        x_backward = x;
+        x_forward(i) = x_forward(i) + delta;
+        x_backward(i) = x_backward(i) - delta;
+
+        J(:, i) = (f(x_forward) - f(x_backward)) / (2 * delta);  % central difference
     end
 end
-
