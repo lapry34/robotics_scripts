5b1ce26c43d18021c0907d0a337bb7bc03e839c1
Gabriele Onorato <onorato.1979543@studenti.uniroma1.it>
2025-03-17 09:47:05 +0100

    better execution

--- Redundancy/projected_gradient.m
+++ Redundancy/projected_gradient.m
@@ -1,7 +1,7 @@
-function q_dot = projected_gradient(J, r_dot, q0_dot) %q0_dot is grad H(q)
+function q_dot = projected_gradient(J, r_dot, q0_dot) %q0_dot is grad H(q) wrt q
    
     I = eye(length(J));
-    q_dot = pinv(J) * r_dot + (I - pinv(J) * J) * q0_dot;
-    %q_dot = q0_dot + pinv(J) * (r_dot - J * q0_dot); %faster version
+    %q_dot = pinv(J) * r_dot + (I - pinv(J) * J) * q0_dot; %original version
+    q_dot = q0_dot + pinv(J) * (r_dot - J * q0_dot); %faster version
     q_dot = simplify(q_dot);
 end
--- Redundancy/task_priority.m
+++ Redundancy/task_priority.m
@@ -13,7 +13,10 @@ function q_dot = task_priority(J1, r1_dot, J2, r2_dot, v2)
 
     J2P1 = simplify(J2 * P1);
     J1_inv_r1_dot = simplify(J1_inv * r1_dot);
+    J2P1_inv = pinv(J2P1);
 
-    q_dot = J1_inv_r1_dot + pinv(J2P1) * (r2_dot - J2  * J1_inv_r1_dot) + P1 * (eye(size(J2P1, 2)) - pinv(J2P1) * J2P1) * v2;
+    %q_dot = J1_inv_r1_dot + J2P1_inv * (r2_dot - J2  * J1_inv_r1_dot) + P1 * (eye(size(J2P1, 2)) - J2P1_inv * J2P1) * v2;
+    q_dot = J1_inv_r1_dot + P1 * J2P1_inv * (r2_dot - J2  * J1_inv_r1_dot) + P1 * (eye(size(J2P1, 2)) - J2P1_inv * J2P1) * v2;
+    %q_dot = J1_inv_r1_dot + P1 * J2P1_inv * (r2_dot - J2  * J1_inv_r1_dot) + P1 * proj_NullSpace(J2P1) * v2;
 
     q_dot = simplify(q_dot);
