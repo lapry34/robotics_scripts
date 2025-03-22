c98b96da136a18b67b93efdc33df1bfa15056607
Gabriele Onorato <onorato.1979543@studenti.uniroma1.it>
2025-03-22 10:56:29 +0100

    Update coordinate_transformation.m

--- Dynamics/coordinate_transformation.m
+++ Dynamics/coordinate_transformation.m
@@ -1,6 +1,6 @@
 
 
-function [M_p, c_p, g_p, u_p] = coordinate_transformation(M, c, g, u, q_dot, J, J_dot)
+function [M_p, c_p, g_p, u_p] = coordinate_transformation(M_q, c_q, g_q, u_q, q_dot, J, J_dot)
     % M: mass matrix
     % c: coriolis matrix
     % g: gravity matrix
@@ -12,11 +12,11 @@ function [M_p, c_p, g_p, u_p] = coordinate_transformation(M, c, g, u, q_dot, J, 
 
     p_dot = J*q_dot;
 
-    M_p = J_inv' * M * J_inv;
-    c_p = J_inv' * c - M_p*J_dot*J_inv*p_dot; % J_inv*p_dot = q_dot? simplify?
-    g_p = J_inv'*g;
+    M_p = J_inv' * M_q * J_inv;
+    c_p = J_inv' * c_q - M_p*J_dot*J_inv*p_dot; % J_inv*p_dot = q_dot? simplify?
+    g_p = J_inv'*g_q;
 
-    u_p = J_inv'*u;
+    u_p = J_inv'*u_q;
 
 end
 
