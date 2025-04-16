6fffe93d30ad472a8c24368dd50b549ff4dfda45
Gabriele Onorato <onorato.1979543@studenti.uniroma1.it>
2025-04-16 12:46:53 +0200

    added nullspace projection

--- Redundancy/minimum_inverse_inertia_weighted_torque_norm_solution.m
+++ Redundancy/minimum_inverse_inertia_weighted_torque_norm_solution.m
@@ -1,16 +1,26 @@
-function tau = minimum_inverse_inertia_weighted_torque_norm_solution(J,  J_dot, q_dot, M, r_ddot, c, g)
+function tau = minimum_inverse_inertia_weighted_torque_norm_solution(J,  J_dot, q_dot, M, r_ddot, n, tau_0)
     %Inputs:
     %   J : (m x n) jacobian matrix of the task 
     %   J_dot : (m x n) time derivative of the jacobian matrix
     %   q_dot : (m x 1) velocity of the joints
     %   M : (n x n) inertia matrix
-    %   c :  (n x 1) Coriolis and centrifugal forces
-    %   g : (n x 1) gravity vector
+    %   n :  (n x 1) Coriolis and centrifugal forces, gravity vector, and other external forces
+    %   tau_0 : (n x 1) nullspace 
     %Output:
     %   tau : (n x 1) minimum (inverse inertia weighted) torque norm solution for dynamic redundancy
     
+    x_ddot = r_ddot - J_dot * q_dot;
     M_inv = inv(M);
-    n = c + g;
-    tau = simplify(J' * inv(J * M_inv * J') * (r_ddot - J_dot * q_dot + J * M_inv * n));
+
+    tau = J' * inv(J * M_inv * J') * (x_ddot + J * M_inv * n);
+
+    if nargin > 6
+        J_inv = pinv(J);
+        num_rows = size(J_inv, 1);
+        I = eye(num_rows);
+        tau = tau + (I - J_inv * J) * tau_0;
+    end
+
+    tau = simplify(tau);
 
 end
